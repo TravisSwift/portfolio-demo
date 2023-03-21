c722221af0bfc1955965f4fb80d6730f5c19314b
Travis Swift <travis.arthur.swift@gmail.com>
2023-03-21 14:22:59 -0500

    added photo to about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
   return (
-    <section>
-      <h1 id="about">Who am I?</h1>
+    <section className='my-5'>
+      <h1 id="about">Travis A. Swift</h1>
+      <h3>A recent graduate of Coding Bootcamp at the University of Texas and seeking to become a junior front end developer or other entry level IT position.
+      Help me become your next best asset.</h3>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
   );
 }
